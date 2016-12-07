b4b1f17db365e38aa197cce207137a1618d6df5c
Brent Yorgey <byorgey@gmail.com>
2016-12-06 19:23:16 -0600

    group tests by name prefix

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,8 +1,11 @@
 module Main where
 
 import           Control.Monad     (filterM)
+import           Data.Char         (isDigit)
+import           Data.Function     (on)
+import           Data.List         (groupBy, sort)
 import           System.Directory  (doesDirectoryExist, getDirectoryContents)
-import           System.FilePath   ((</>))
+import           System.FilePath   (isPathSeparator, (</>))
 import           System.Process    (system)
 
 import           Test.Tasty
@@ -11,14 +14,26 @@ import           Test.Tasty.Golden
 main :: IO ()
 main = do
   testDirs <- getDirectoryContents "test" >>= filterM (doesDirectoryExist . ("test"</>))
-  let testDirs' = filter (\f -> f /= "." && f /= "..") testDirs
-  let testTree = testGroup "disco" $ map mkGolden testDirs'
+  let testDirs'
+        = groupBy ((==) `on` extractGroup)
+        . sort
+        . filter (\f -> f /= "." && f /= "..")
+        $ testDirs
+  let testTree = testGroup "disco" $ map mkGroup testDirs'
   defaultMain testTree
+  where
+    mkGroup ds@(d:_) = testGroup (extractGroup d) $ map mkGolden ds
+
+extractGroup :: FilePath -> String
+extractGroup = reverse . dropWhile isDigit . takeWhile (not . isPathSeparator) . reverse
+
+extractNum :: FilePath -> String
+extractNum = reverse . takeWhile isDigit . reverse
 
 mkGolden :: FilePath -> TestTree
 mkGolden relDir =
   goldenVsFile
-    relDir
+    (extractNum relDir)
     (dir </> "expected")
     (dir </> "output")
     (system ("disco -f " ++ (dir </> "input") ++ " > " ++ (dir </> "output")) >> return ())

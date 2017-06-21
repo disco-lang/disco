f96f83fea3baba55581b6d7ca9698accf0f50542
Brent Yorgey <byorgey@gmail.com>
2017-06-20 20:33:45 -0500

    move TODO comment into a github issue (#17)

--- src/Disco/Typecheck.hs
+++ src/Disco/Typecheck.hs
@@ -639,11 +639,6 @@ inferGuard (GPat (unembed -> t) p) = do
   ctx <- checkPattern p (getType at)
   return (AGPat (embed at) p, ctx)
 
--- XXX todo: check for nonlinear patterns.
--- Nonlinear patterns can desugar to equality checks!
--- Currently  { x when (3,4) = (x,x)   evaluates without error to 3.
--- It should be accepted, but fail to match since 3 != 4.
-
 -- | Check that a pattern has the given type, and return a context of
 --   pattern variables bound in the pattern along with their types.
 checkPattern :: Pattern -> Type -> TCM Ctx

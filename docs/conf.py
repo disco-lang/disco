ad66cf3f0542eae3e848844177d37d016e814213
Brent Yorgey <byorgey@gmail.com>
2018-04-24 14:28:02 -0500

    docs: update copyright year [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'disco'
-copyright = u'2017, disco team'
+copyright = u'2017-2018, disco team'
 author = u'disco team'
 
 # The version info for the project you're documenting, acts as replacement for

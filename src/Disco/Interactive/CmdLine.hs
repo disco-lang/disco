782baf1f0c039a50b46df179dd09434fc120b64c
Brent Yorgey <byorgey@gmail.com>
2019-02-28 15:02:29 -0600

    refactor: split up REPL into several modules in the library

---
+++ src/Disco/Interactive/CmdLine.hs
@@ -0,0 +1,144 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Disco.Interactive.CmdLine
+-- Copyright   :  disco team and contributors
+-- Maintainer  :  byorgey@gmail.com
+--
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- Expression type and parser for things entered at an interactive disco
+-- prompt.
+--
+-----------------------------------------------------------------------------
+
+module Disco.Interactive.CmdLine
+  ( -- * Command-line options record
+
+    DiscoOpts(..)
+
+    -- * optparse-applicative command line parsers
+  , discoOpts, discoInfo
+
+    -- * main
+
+  , discoMain
+
+  ) where
+
+import           Control.Monad             (when)
+import           Control.Monad.IO.Class    (MonadIO (..))
+import           Control.Monad.Trans.Class (MonadTrans (..))
+import           Data.List                 (isPrefixOf)
+import           Data.Maybe                (isJust)
+import           System.Exit               (exitFailure, exitSuccess)
+
+import qualified Options.Applicative       as O
+import           System.Console.Haskeline  as H
+
+import           Disco.Eval
+import           Disco.Interactive.Eval
+
+------------------------------------------------------------
+-- XXX
+------------------------------------------------------------
+
+banner :: String
+banner = "Welcome to Disco!\n\nA language for programming discrete mathematics.\n\n"
+
+data DiscoOpts = DiscoOpts
+  { evaluate  :: Maybe String
+  , cmdFile   :: Maybe String
+  , checkFile :: Maybe String
+  }
+
+discoOpts :: O.Parser DiscoOpts
+discoOpts = DiscoOpts
+  <$> O.optional (
+        O.strOption (mconcat
+          [ O.long "evaluate"
+          , O.short 'e'
+          , O.help "evaluate an expression"
+          , O.metavar "TERM"
+          ])
+      )
+  <*> O.optional (
+        O.strOption (mconcat
+          [ O.long "file"
+          , O.short 'f'
+          , O.help "execute the commands in a file"
+          , O.metavar "FILE"
+          ])
+      )
+  <*> O.optional (
+        O.strOption (mconcat
+          [ O.long "check"
+          , O.help "check a file without starting the interactive REPL"
+          , O.metavar "FILE"
+          ])
+      )
+
+discoInfo :: O.ParserInfo DiscoOpts
+discoInfo = O.info (O.helper <*> discoOpts) $ mconcat
+  [ O.fullDesc
+  , O.progDesc "Command-line interface for Disco, a programming language for discrete mathematics."
+  , O.header "disco v0.1"
+  ]
+
+discoMain :: IO ()
+discoMain = do
+  opts <- O.execParser discoInfo
+
+  let batch = any isJust [evaluate opts, cmdFile opts, checkFile opts]
+      settings = defaultSettings
+            { historyFile = Just ".disco_history" }
+  when (not batch) $ putStr banner
+  res <- runDisco $ do
+    case checkFile opts of
+      Just file -> do
+        res <- handleLoad file
+        io $ if res then exitSuccess else exitFailure
+      Nothing   -> return ()
+    case cmdFile opts of
+      Just file -> do
+        mcmds <- loadFile file
+        case mcmds of
+          Nothing   -> return ()
+          Just cmds -> mapM_ handleCMD (lines cmds)
+      Nothing   -> return ()
+    case evaluate opts of
+      Just str -> handleCMD str
+      Nothing  -> return ()
+
+    when (not batch) $ runInputT settings loop
+
+  case res of
+
+    -- All disco exceptions should be caught and handled by this point.
+    Left e   -> do
+      putStrLn $ "Uncaught error: " ++ show e
+      putStrLn $ "Please report this as a bug: https://github.com/disco-lang/disco/issues"
+    Right () -> return ()
+
+  -- XXX pretty-print log messages here
+
+  where
+
+    ctrlC :: InputT (Disco e) a -> SomeException -> InputT (Disco e) a
+    ctrlC act e = do
+      io $ putStrLn (show e)
+      act
+
+    withCtrlC resume act = H.catch (H.withInterrupt act) (ctrlC resume)
+
+    loop :: InputT (Disco IErr) ()
+    loop = do
+      minput <- withCtrlC (return $ Just "") (getInputLine "Disco> ")
+      case minput of
+        Nothing -> return ()
+        Just input
+          | ":q" `isPrefixOf` input && input `isPrefixOf` ":quit" -> do
+              liftIO $ putStrLn "Goodbye!"
+              return ()
+          | otherwise -> do
+              withCtrlC (return ()) $ (lift . handleCMD $ input)
+              loop
---
+++ src/Disco/Interactive/Parser.hs
@@ -0,0 +1,97 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Disco.Interactive.Parser
+-- Copyright   :  disco team and contributors
+-- Maintainer  :  byorgey@gmail.com
+--
+-- SPDX-License-Identifier: BSD-3-Clause
+--
+-- Expression type and parser for things entered at an interactive disco
+-- prompt.
+--
+-----------------------------------------------------------------------------
+
+module Disco.Interactive.Parser
+  ( REPLExpr(..)
+  , letParser, commandParser, parseCommandArgs, fileParser, lineParser, parseLine
+  ) where
+
+import           Data.Char                        (isSpace)
+import           Data.List                        (find, isPrefixOf)
+
+import           Text.Megaparsec                  hiding (runParser)
+import qualified Text.Megaparsec.Char             as C
+import           Unbound.Generics.LocallyNameless
+
+import           Disco.AST.Surface
+import           Disco.Extensions
+import           Disco.Parser
+
+------------------------------------------------------------
+-- REPL expression type
+------------------------------------------------------------
+
+data REPLExpr =
+   Using Ext                    -- Enable an extension
+ | Let (Name Term) Term         -- Toplevel let-expression: for the REPL
+ | TypeCheck Term               -- Typecheck a term
+ | Eval Term                    -- Evaluate a term
+ | ShowDefn (Name Term)         -- Show a variable's definition
+ | Parse Term                   -- Show the parsed AST
+ | Pretty Term                  -- Pretty-print a term
+ | Ann Term                     -- Show type-annotated typechecked term
+ | Desugar Term                 -- Show a desugared term
+ | Compile Term                 -- Show a compiled term
+ | Load FilePath                -- Load a file.
+ | Reload                       -- Reloads the most recently loaded file.
+ | Doc (Name Term)              -- Show documentation.
+ | Nop                          -- No-op, e.g. if the user just enters a comment
+ | Help
+ deriving Show
+
+------------------------------------------------------------
+-- Parser
+------------------------------------------------------------
+
+letParser :: Parser REPLExpr
+letParser = Let
+  <$> ident
+  <*> (symbol "=" *> term)
+
+commandParser :: Parser REPLExpr
+commandParser = (symbol ":" *> many C.lowerChar) >>= parseCommandArgs
+
+parseCommandArgs :: String -> Parser REPLExpr
+parseCommandArgs cmd = maybe badCmd snd $ find ((cmd `isPrefixOf`) . fst) parsers
+  where
+    badCmd = fail $ "Command \":" ++ cmd ++ "\" is unrecognized."
+    parsers =
+      [ ("type",    TypeCheck <$> term)
+      , ("defn",    ShowDefn  <$> (sc *> ident))
+      , ("parse",   Parse     <$> term)
+      , ("pretty",  Pretty    <$> term)
+      , ("ann",     Ann       <$> term)
+      , ("desugar", Desugar   <$> term)
+      , ("compile", Compile   <$> term)
+      , ("load",    Load      <$> fileParser)
+      , ("reload",  return Reload)
+      , ("doc",     Doc       <$> (sc *> ident))
+      , ("help",    return Help)
+      ]
+
+fileParser :: Parser FilePath
+fileParser = many C.spaceChar *> many (satisfy (not . isSpace))
+
+lineParser :: Parser REPLExpr
+lineParser
+  =   commandParser
+  <|> try (Nop <$ (sc <* eof))
+  <|> try (Using <$> (reserved "using" *> parseExtName))
+  <|> try (Eval <$> term)
+  <|> letParser
+
+parseLine :: ExtSet -> String -> Either String REPLExpr
+parseLine exts s =
+  case (runParser (withExts exts lineParser) "" s) of
+    Left  e -> Left $ errorBundlePretty e
+    Right l -> Right l

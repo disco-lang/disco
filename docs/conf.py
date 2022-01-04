a619190584987c5f56b0e4e43746a1334e1ccb64
Brent Yorgey <byorgey@gmail.com>
2022-01-04 10:39:15 -0600

    docs: set master_doc to index
    See https://github.com/readthedocs/readthedocs.org/issues/2569

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ author = 'disco team'
 # The full version, including alpha/beta/rc tags
 release = '1.0'
 
+master_doc = 'index'
+
 
 # -- General configuration ---------------------------------------------------
 

864e98c359cd3c7e7d2117193121c9b547316ed1
Brent Yorgey <byorgey@gmail.com>
2023-10-12 11:55:30 -0500

    WIP

--- src/Disco/Error.hs
+++ src/Disco/Error.hs
@@ -19,8 +19,6 @@ module Disco.Error (
 
 import Prelude hiding ((<>))
 
-import Unbound.Generics.LocallyNameless (Name)
-
 import Disco.Effects.LFresh
 import Polysemy
 import Polysemy.Error
@@ -28,14 +26,9 @@ import Polysemy.Output
 import Polysemy.Reader
 
 import Disco.Messages
-import Disco.Names (ModuleName, QName)
+import Disco.Names (ModuleName)
 import Disco.Pretty
 import Disco.Typecheck.Solve
-import Disco.Typecheck.Util (
-  LocTCError (..),
-  TCError (..),
- )
-import Disco.Types
 import Disco.Types.Qualifiers
 
 -- | Top-level error type for Disco.
@@ -61,7 +54,7 @@ data DiscoError = DiscoError
   -- location info?
   , errHints :: [Doc]
   -- ^ Things to try, examples, etc. that might help.
-  , errReading :: [Doc]
+  , errReading :: [FurtherReading]
   -- ^ References to further reading.
   }
 
@@ -72,6 +65,15 @@ data DiscoErrorKind
   | ParseErr
   | EvalErr
   | Panic
+
+-- | Sources for further reading.
+data FurtherReading
+  = -- | Link to documentation page on readthedocs.
+    RTD String
+  | -- | Link to a GitHub issue.
+    Issue Int
+  | -- | Free-form.
+    OtherReading Doc
 
 panic :: Member (Error DiscoError) r => Maybe Int -> String -> Sem r a
 panic issueNum panicMsg = do
@@ -134,153 +136,6 @@ cyclicImportError ms =
     [ "Error: module imports form a cycle:"
     , nest 2 $ intercalate " ->" (map pretty ms)
     ]
-
--- [X] Step 1: nice error messages, make sure all are tested
--- [ ] Step 2: link to wiki/website with more info on errors!
--- [ ] Step 3: improve error messages according to notes below
--- [ ] Step 4: get it to return multiple error messages
--- [ ] Step 5: save parse locations, display with errors
-prettyTCError :: Members '[Reader PA, LFresh] r => TCError -> Sem r Doc
-prettyTCError = \case
-  -- XXX include some potential misspellings along with Unbound
-  --   see https://github.com/disco-lang/disco/issues/180
-  Unbound x ->
-    vcat
-      [ "Error: there is nothing named" <+> pretty' x <> "."
-      , rtd "unbound"
-      ]
-  Ambiguous x ms ->
-    vcat
-      [ "Error: the name" <+> pretty' x <+> "is ambiguous. It could refer to:"
-      , nest 2 (vcat . map (\m -> pretty' m <> "." <> pretty' x) $ ms)
-      , rtd "ambiguous"
-      ]
-  NoType x ->
-    vcat
-      [ "Error: the definition of" <+> pretty' x <+> "must have an accompanying type signature."
-      , "Try writing something like '"
-          <> pretty' x
-          <+> ": Int' (or whatever the type of"
-          <+> pretty' x
-          <+> "should be) first."
-      , rtd "missingtype"
-      ]
-  NotCon c t ty ->
-    vcat
-      [ "Error: the expression"
-      , nest 2 $ pretty' t
-      , "must have both a" <+> conWord c <+> "type and also the incompatible type"
-      , nest 2 $ pretty' ty <> "."
-      , rtd "notcon"
-      ]
-  EmptyCase ->
-    vcat
-      [ "Error: empty case expressions {? ?} are not allowed."
-      , rtd "empty-case"
-      ]
-  PatternType c pat ty ->
-    vcat
-      [ "Error: the pattern"
-      , nest 2 $ pretty' pat
-      , "is supposed to have type"
-      , nest 2 $ pretty' ty <> ","
-      , "but instead it has a" <+> conWord c <+> "type."
-      , rtd "pattern-type"
-      ]
-  DuplicateDecls x ->
-    vcat
-      [ "Error: duplicate type signature for" <+> pretty' x <> "."
-      , rtd "dup-sig"
-      ]
-  DuplicateDefns x ->
-    vcat
-      [ "Error: duplicate definition for" <+> pretty' x <> "."
-      , rtd "dup-def"
-      ]
-  DuplicateTyDefns s ->
-    vcat
-      [ "Error: duplicate definition for type" <+> text s <> "."
-      , rtd "dup-tydef"
-      ]
-  -- XXX include all types involved in the cycle.
-  CyclicTyDef s ->
-    vcat
-      [ "Error: cyclic type definition for" <+> text s <> "."
-      , rtd "cyc-ty"
-      ]
-  -- XXX lots more info!  & Split into several different errors.
-  NumPatterns ->
-    vcat
-      [ "Error: number of arguments does not match."
-      , rtd "num-args"
-      ]
-  NonlinearPattern p x ->
-    vcat
-      [ "Error: pattern" <+> pretty' p <+> "contains duplicate variable" <+> pretty' x <> "."
-      , rtd "nonlinear"
-      ]
-  NoSearch ty ->
-    vcat
-      [ "Error: the type"
-      , nest 2 $ pretty' ty
-      , "is not searchable (i.e. it cannot be used in a forall)."
-      , rtd "no-search"
-      ]
-  Unsolvable solveErr -> prettySolveError solveErr
-  -- XXX maybe include close edit-distance alternatives?
-  NotTyDef s ->
-    vcat
-      [ "Error: there is no built-in or user-defined type named '" <> text s <> "'."
-      , rtd "no-tydef"
-      ]
-  NoTWild ->
-    vcat
-      [ "Error: wildcards (_) are not allowed in expressions."
-      , rtd "wildcard-expr"
-      ]
-  -- XXX say how many are expected, how many there were, what the actual arguments were?
-  -- XXX distinguish between built-in and user-supplied type constructors in the error
-  --     message?
-  NotEnoughArgs con ->
-    vcat
-      [ "Error: not enough arguments for the type '" <> pretty' con <> "'."
-      , rtd "num-args-type"
-      ]
-  TooManyArgs con ->
-    vcat
-      [ "Error: too many arguments for the type '" <> pretty' con <> "'."
-      , rtd "num-args-type"
-      ]
-  -- XXX Mention the definition in which it was found, suggest adding the variable
-  --     as a parameter
-  UnboundTyVar v ->
-    vcat
-      [ "Error: Unknown type variable '" <> pretty' v <> "'."
-      , rtd "unbound-tyvar"
-      ]
-  NoPolyRec s ss tys ->
-    vcat
-      [ "Error: in the definition of " <> text s <> parens (intercalate "," (map text ss)) <> ": recursive occurrences of" <+> text s <+> "may only have type variables as arguments."
-      , nest
-          2
-          ( text s <> parens (intercalate "," (map pretty' tys)) <+> "does not follow this rule."
-          )
-      , rtd "no-poly-rec"
-      ]
-  NoError -> empty
-
-conWord :: Con -> Sem r Doc
-conWord = \case
-  CArr -> "function"
-  CProd -> "pair"
-  CSum -> "sum"
-  CSet -> "set"
-  CBag -> "bag"
-  CList -> "list"
-  CContainer _ -> "container"
-  CMap -> "map"
-  CGraph -> "graph"
-  CUser s -> text s
 
 prettySolveError :: Members '[Reader PA, LFresh] r => SolveError -> Sem r Doc
 prettySolveError = \case
